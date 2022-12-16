bb377a444bb680162d947310692d47132192cdf4
Emil <emil94442@gmail.com>
2022-12-16 16:56:27 +0600

    feat: add create-post modal

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -3,8 +3,62 @@ import './CreatePost.scss'
 
 const CreatePost = () => {
   return (
-    <div className='CreatePost'>
-        
+    <div className='cp'>
+        <div className="cp__content">
+            <div className="cp__x-img">
+                <img src="Images/createPost/x.svg" alt="" />
+            </div>
+            <div className="cp__download-content">
+                <div className="cp__form-block-one">
+                    <div className="cp__form-text">
+                        Обложка новости
+                    </div>
+                    <div className="cp__download-button">
+                        <img src="Images/createPost/download-button.svg" alt="" />
+                    </div>
+                </div>
+                <div className="cp__form-block-one">
+                    <div className="cp__form-text">
+                       Заголовок
+                    </div>
+                    <div className="cp__input-content">
+                        <input type="text" className='cp__input'/>
+                    </div>
+                </div>
+                <div className="cp__form-block-one">
+                    <div className="cp__form-text">
+                       Краткое описание
+                    </div>
+                    <div className="cp__input-content">
+                        <input type="text" className='cp__input'/>
+                    </div>
+                </div>
+                <div className="cp__form-block-one">
+                    <div className="cp__form-text">
+                       Текст новости
+                    </div>
+                    <div className="cp__input-content">
+                        <input type="text" className='cp__input'/>
+                    </div>
+                </div>
+                <div className="cp__form-block-one">
+                    <div className="cp__form-text">
+                       Выбрать категорию
+                    </div>
+                    <div className="cp__select-content">
+                        <select>
+                            <option value="not-selected">Не выбрано</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="cp__not-selected-button">
+                    #не выбрано
+                </div>
+                <div className="cp__button-creater">
+                    Создать
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
