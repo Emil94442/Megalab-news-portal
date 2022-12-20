2c84098226bb8aeb85de4d0cdb01ee77bfadf555
Emil <emil94442@gmail.com>
2022-12-20 23:50:34 +0600

    report: 19_12_2022

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ProfilePage from './Components/ProfilePage/ProfilePage';
 import CreatePost from './Components/CreatePost/CreatePost';
 import Autorization from './Components/authorization/Autorization';
 import InnerPage from './Components/InnerPage/InnerPage';
+import { useEffect } from 'react';
 
 function App() {
 
@@ -25,6 +26,51 @@ function App() {
       setValueModal(value)
     }
   }
+
+  // token: 55bebfbe8e8532494dbe49f56f5c39fc2744aace
+
+  const [posts , setPosts] = useState([])
+
+  const loadDate = async () => {
+    const body = {
+      'nickname': 'Emil_GD',
+      'name': 'Emil',
+      'last_name': 'Turd',
+      'profile_image': null,
+      'password': '123_qwerty',
+      'password2': '123_qwerty'
+    }
+
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+    
+    const data = await fetch('https://megalab.pythonanywhere.com/registration/', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: headers
+    })
+    const response = await data.json()
+    if (response.ok) {
+      setPosts(response)
+    }
+
+    /*.then(response => response.json())
+    .then(json => setPosts(json))*/
+    console.log(posts)
+
+    
+
+  }
+
+
+
+
+  useEffect(() => {
+      loadDate()
+  }, [])
+
+  console.log(posts)
 
   return (
     <div className="App">
